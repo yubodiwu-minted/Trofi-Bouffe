763f84da586b9b14d5b04c22a15683f4c97cc6cc
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-01-30 15:06:00 -0800

    hashed passwords before posting users to database

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const knex = require("../knex");
+const bcrypt = require("bcrypt-as-promised")
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
     console.log("users get / route is hit");
-    // res.send("balls");
-    res.render('../views/index.ejs')
+    res.render("../views/index.ejs")
 });
 
 router.post("/", function(req, res) {
     console.log("users post / route is hit");
 
-    knex("users").insert({
-        first_name: req.body.firstName,
-        last_name: req.body.lastName,
-        email: req.body.email,
-        username: req.body.username,
-        hashed_password: req.body.password
+    bcrypt.hash(req.body.password, 12).then((hashedPassword) => {
+        return hashedPassword;
+    })
+    .then((hashedPassword) => {
+        return knex("users").insert({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            username: req.body.username,
+            hashed_password: hashedPassword
+        });
     })
     .then((data) => {
         console.log(data);
     })
     .catch((error) => {
-        console.error(error);
+        console.log(error);
     });
 });
 
