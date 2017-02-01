74baa613e0ebdd92f3fe26c3268ed3b3c4b380f4
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-02-01 14:28:55 -0800

    finished seeding ingredients and amounts for cacio e pepe

--- seeds/04_ingredients.js
+++ seeds/04_ingredients.js
@@ -1,16 +1,32 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("ingredients").del()
-    .then(function () {
-      return knex("recipe_ingredients").insert([
-          {
-              id: 98,
-              upc: "782645787019",
-              plu: null,
-              product_name: "Kirkland Organice Extra Virgin Olive Oil 3.6 fl oz.",
-              name: "extra-virgin olive oil"
-          }
-      ]);
+    // Deletes ALL existing entries
+    return knex("ingredients").del().then(function() {
+        return knex("recipe_ingredients").insert([
+            {
+                id: 98,
+                upc: "782645787019",
+                plu: null,
+                product_name: "Kirkland Organic Extra Virgin Olive Oil 3.6 fl oz.",
+                name: "extra-virgin olive oil"
+            }, {
+                id: 99,
+                upc: "096619261000",
+                plu: null,
+                product_name: "Kirkland Signature 6.3 Oz Tellicherry Black Pepper Grinder",
+                name: "black pepper"
+            }, {
+                id: 100,
+                upc: "013600020019",
+                plu: null,
+                product_name: "Diamond Crystal Kosher Salt",
+                name: "kosher salt"
+            }, {
+                id: 101,
+                upc: "234779811561",
+                plu: null,
+                product_name: "Kirkland Signature Costco Pecorino R Romano",
+                name: "pecorino romano cheese"
+            }
+        ]);
     });
 };
--- seeds/05_recipe_ingredients.js
+++ seeds/05_recipe_ingredients.js
@@ -1,28 +1,38 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("recipe_ingredients").del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex("recipe_ingredients").insert({
-            recipe_id: 1,
-            ingredient_id: 48,
-            amount: 2,
-            units: "tbs"
-        }),
-        knex("recipe_ingredients").insert({
-            recipe_id: 1,
-            ingredient_id: 67,
-            amount: 0.5,
-            units: "lbs"
-        }),
-        knex("recipe_ingredients").insert({
-            recipe_id: 1,
-            ingredient_id: 98,
-            amount: 4,
-            units: "tbs"
-        })
-      ]);
+    // Deletes ALL existing entries
+    return knex("recipe_ingredients").del().then(function() {
+        return knex("recipe_ingredients").insert([
+            {
+                recipe_id: 1,
+                ingredient_id: 48,
+                amount: 2,
+                units: "tbs"
+            }, {
+                recipe_id: 1,
+                ingredient_id: 67,
+                amount: 0.5,
+                units: "lbs"
+            }, {
+                recipe_id: 1,
+                ingredient_id: 98,
+                amount: 4,
+                units: "tbs"
+            }, {
+                recipe_id: 1,
+                ingredient_id: 99,
+                amount: 1,
+                units: "tsp"
+            }, {
+                recipe_id: 1,
+                ingredient_id: 100,
+                amount: null,
+                units: null
+            }, {
+                recipe_id: 1,
+                ingredient_id: 101,
+                amount: 2,
+                units: "oz"
+            }
+        ]);
     });
 };
