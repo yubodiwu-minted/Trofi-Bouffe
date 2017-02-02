a39373e299fb53274b89c8cb3ad41ba08760064d
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-02-01 21:22:51 -0800

    added recipe servings and calories to migrations and servings to cacio e pepe recipe seed

--- migrations/20170128000003_sample.js
+++ migrations/20170128000003_sample.js
@@ -14,6 +14,8 @@ exports.up = function(knex, Promise) {
             table.increments("id").primary();
             table.integer("user_id").notNullable().references("id").inTable("users").onDelete("cascade");
             table.string("name").notNullable();
+            table.integer("calories");
+            table.integer("servings");
             table.string("img");
             table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
             table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
