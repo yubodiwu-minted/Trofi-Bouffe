2ded6d675aea2e2f7ebb377c6ca543cc952d0bf9
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-02-01 18:09:22 -0800

    got recipe image and name onto page

--- app/components/RecipeView.jsx
+++ app/components/RecipeView.jsx
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 var renderIngredients = (props) => {
+    if (!props.currentRecipe) {
+        return <h1>No recipe specified...</h1>
+    }
+    
     var key = 0
 
     return props.ingredientsList.map((ingredient) => {
@@ -17,6 +21,10 @@ var RecipeView = (props) => {
     return (
         <div className="content-container row">
             <div className="content-list columns medium-10 large-8 small-centered">
+                <div>
+                    <img src={props.currentRecipe.img} alt="recipe_img"/>
+                    <h3>{props.currentRecipe.name}</h3>
+                </div>
                 <div className="recipes-holder">
                     {renderIngredients(props)}
                 </div>
