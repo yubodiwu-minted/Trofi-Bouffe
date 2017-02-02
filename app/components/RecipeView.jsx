99db9865a5c2d4dcd0fe0bde5296c75eaacf1c8b
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-02-01 17:25:05 -0800

    made ingredients appear on recipe view

--- app/components/RecipeView.jsx
+++ app/components/RecipeView.jsx
@@ -1,14 +1,32 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+
+var renderIngredients = (props) => {
+    var key = 0
+
+    return props.ingredientsList.map((ingredient) => {
+        return (
+            <p key={key++}>
+                {ingredient.name}
+            </p>
+        );
+    })
+}
 
 var RecipeView = (props) => {
     return (
         <div className="content-container row">
             <div className="content-list columns medium-10 large-8 small-centered">
                 <div className="recipes-holder">
+                    {renderIngredients(props)}
                 </div>
             </div>
         </div>
     );
 }
 
-export default RecipeView;
+export default connect((state) => {
+    return {
+        ingredientsList: state.ingredientsList
+    };
+})(RecipeView);
