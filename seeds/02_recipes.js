8f2472da0f49040bcd1b8c974fa851c8777ba7d4
Yubo Allan Diwu <yubo.diwu@gmail.com>
2017-02-01 20:31:47 -0800

    cleaned up seed data and actions a bit, deleting comments and adding semi-colons

--- seeds/02_recipes.js
+++ seeds/02_recipes.js
@@ -1,7 +1,6 @@
 exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
     return knex("recipes").del().then(function() {
-        return Promise.all([// Inserts seed entries
+        return Promise.all([
             knex("recipes").insert({
                 id: 1,
                 user_id: 5,
--- seeds/05_recipe_ingredients.js
+++ seeds/05_recipe_ingredients.js
@@ -1,14 +1,6 @@
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
     return knex("recipe_ingredients").del().then(function() {
-        // return Promise.all([
-        //     knex("recipe_ingredients").insert({
-        //         recipe_id: 1,
-        //         ingredient_id: 48,
-        //         quantity: 2,
-        //         units: "tbs"
-        //     })
-        // ])
         return knex("recipe_ingredients").insert([
             {
                 recipe_id: 1,
